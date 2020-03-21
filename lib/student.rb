e47a8bc0c9ce2acbf59e15162ab781e13449fcc1
Maynard Hartman <mjhartman@brighthouse.com>
2020-03-21 14:33:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,47 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  attr_accessor :name, :grade
+  attr_reader :id
   
+  def initialize(name, grade)
+    @name = name
+    @grade = grade 
+    @id = nil
+    
+  end
+  
+  def self.create_table
+    sql = "CREATE TABLE students(
+      id    INT NOT NULL,
+      name  STRING,
+      grade STRING, 
+      PRIMARY KEY(id))"
+    DB[:conn].execute(sql)
+    
+  end  
+  
+  def self.drop_table
+    sql = "DROP TABLE students"
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = "INSERT INTO students (name, grade) VALUES (@name, @grade)"
+    result = DB[:conn].execute(sql)
+    @id = result
+    binding.pry
+    puts ""
+  end
+  
+  def self.create(new_name, new_grade)
+    new_student = Student.new(new_name, new_grade)
+    save(name, grade)
+    binding.pry
+    return(new_student)
+  end
+   
+  def id
+    @id 
+  end
 end
